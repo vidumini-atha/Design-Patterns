a02d617d1be925efeccc564f355716630855039c
Vidumini Athapaththu <viduathapaththu@gmail.com>
2025-01-27 00:08:54 +0530

    Add files via upload

---
+++ AdapterPattern/src/MallardDuck.java
@@ -0,0 +1,10 @@
+public class MallardDuck implements Duck {
+    public void quack() {
+        System.out.println("Quack quack...");
+    }
+
+    public void fly() {
+        System.out.println("Fly...");
+    }
+
+}
---
+++ AdapterPattern/src/WildTurkey.java
@@ -0,0 +1,9 @@
+public class WildTurkey implements Turkey {
+    public void gobble() {
+        System.out.println("Gobble gobble...");
+    }
+
+    public void fly() {
+        System.out.println("Short distance flight...");
+    }
+}
